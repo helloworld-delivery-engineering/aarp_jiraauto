ccaf4abfaffb0e0ff7a6d2e8496973f203e33538
Mike Work <42551656+mikework-hw@users.noreply.github.com>
2022-07-20 10:57:25 -0400

    Create RCI_JirAuto.js
    Initial Commit

---
+++ RCI_JirAuto.js
@@ -0,0 +1,16 @@
+// == AARP Rewards Catalog Item - Jira Auto ==
+// @name         Jira Auto
+// @namespace    https://jiradc.helloworld.com/
+// @version      0.1
+// @description  Efficiently and accurately creating new Rewards Catalog Item Jira tickets
+// @author       Colby Lostutter and the Blue Workstream
+// @match        https://jiradc.helloworld.com/*
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    alert("Tampermonkey Jira Auto Script Active!");
+})();
