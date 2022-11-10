40223c7dca0b3f4775bae82ce7904bd858a3b016
Colby Lostutter <colby.lostutter@helloworld.com>
2022-11-10 08:09:47 -0500

    changed from 1.5 to 1.6

--- RCI_JirAuto.js
+++ RCI_JirAuto.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Jira Auto
 // @namespace    https://jiradc.helloworld.com/
-// @version      1.5
+// @version      1.6
 // @description  Efficiently and accurately creating new Rewards Catalog Item Jira tickets
 // @author       Colby Lostutter and the Blue Workstream
 // @match        https://jiradc.helloworld.com/*
